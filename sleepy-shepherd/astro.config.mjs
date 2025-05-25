10a9523d8b7e53ddf16226fe720e86f885df26d7
Irakli Popkhadze <iraklipopkhadze@Iraklis-MacBook-Pro.local>
2025-05-25 17:13:30 +0100

    remove sleepy-shepherd folder and move contents

--- sleepy-shepherd/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-	site: 'https://ilakli/github.io',
-	output: 'static',
-});
--- sleepy-shepherd/src/consts.ts
+++
@@ -1,2 +0,0 @@
-export const SITE_TITLE = 'Iraklis Blog';
-export const SITE_DESCRIPTION = 'Welcome to my blog!';
